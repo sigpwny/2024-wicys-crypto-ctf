f87f46f1ca10e69217598c2563e3a98bdd5dae94
Sophia Liu <sophialiu7436@gmail.com>
2024-03-12 13:38:51 -0500

    Update challenges

--- chals/crypto/seal_trouble_1/chal.py
+++ chals/crypto/seal_trouble_1/chal.py
@@ -2,9 +2,9 @@ def encrypt(message, key):
     return bytes([b ^ key[i % len(key)] for i, b in enumerate(message)])
 
 def main():
-    passphrase = input("Enter passphrase: ")
+    passphrase = input("Enter secret key: ")
     key = [ord(char) for char in passphrase]
-    message = b"sigpwny{fake_flag}"
+    message = b"sigpwny{not_the_real_flag}"
     
     # Encrypt
     ciphertext = encrypt(message, key)
--- chals/crypto/seal_trouble_1/sol.py
+++ chals/crypto/seal_trouble_1/sol.py
@@ -1,12 +1,12 @@
 # Step 1: Inspect "comment" tag in metadata for passphrase (ex: exiftool seal1.png)
-passphrase = "tomatoisafruit"
+passphrase = "morefishplz"
 
 # Step 2:
 def decrypt(ciphertext, key):
     return bytes([b ^ key[i % len(key)] for i, b in enumerate(ciphertext)])
 
 # Provided from message.txt
-input = "07060a110301100806030601001a13301e15151d1d160539052a11440612"
+input = "1e06151511070a1303091b011c2d090f021637085c0810"
 
 # Convert hexadecimal string to bytes
 b_input = bytes.fromhex(input)
